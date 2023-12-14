1507bdc2cac3880e51c3fda06c772d42f42d8c0d
EkremDincel <ekremdincel@gmail.com>
2023-12-14 17:19:20 +0300

    Update user agent

--- source/conf.py
+++ source/conf.py
@@ -91,5 +91,5 @@ latex_additional_files = [cover, customcls]
 
 # other
 today_fmt = html_last_updated_fmt
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
 
